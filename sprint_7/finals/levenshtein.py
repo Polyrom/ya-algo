ca003b4ceb2f641bbc288c38a88ac0f44c5e633b
Roman Poliakov <roman61394@gmail.com>
2025-06-10 18:31:29 +0300

    sprint 7 finals 3 (optimization)

--- sprint_7/finals/levenshtein.py
+++ sprint_7/finals/levenshtein.py
@@ -4,23 +4,23 @@ def levenshtein(a: str, b: str) -> int:
     if len(b) == 0:
         return len(a)
 
-    n = len(a)
-    m = len(b)
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    n, m = len(a), len(b)
 
-    for i in range(0, n + 1):
-        dp[i][0] = i
-    for i in range(0, m + 1):
-        dp[0][i] = i
+    prev = list(range(m + 1))
+    curr = [0] * (m + 1)
 
     for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            insertion = dp[i - 1][j] + 1
-            deletion = dp[i][j - 1] + 1
-            substitution = dp[i - 1][j - 1] + (1 if a[i - 1] != b[j - 1] else 0)
-            dp[i][j] = min(insertion, deletion, substitution)
+        curr[0] = i
 
-    return dp[n][m]
+        for j in range(1, m + 1):
+            insertion = prev[j] + 1
+            deletion = curr[j - 1] + 1
+            substitution = prev[j - 1] + (0 if a[i - 1] == b[j - 1] else 1)
+            curr[j] = min(insertion, deletion, substitution)
+
+        prev, curr = curr, prev
+
+    return prev[m]
 
 
 if __name__ == "__main__":
